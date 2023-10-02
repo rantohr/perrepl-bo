9fd3f3b23b92400ae5e3b7a7c636c696fdcd5704
Andrianina <araherimanantsoa@bocasay.com>
2023-10-02 10:47:34 +0300

    Init Project

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="p-4 m-auto border-2 rounded-lg bg-slate-800 text-white">
+        <h1 className="text-3xl font-bold">Hello world!</h1>
+        <p className="mt-5">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam qui
+          minima autem nobis animi perspiciatis deleniti quaerat, recusandae
+          officiis ea totam quisquam repellat quidem perferendis ad! Vel officia
+          voluptatibus ipsam.
+        </p>
+        <button className="bg-indigo-600 hover:bg-indigo-800 mt-4 py-2 px-4 rounded">
+          Click Me
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
