f285948a7248c18268af7b032d13ee12be7791c6
Andrianina <araherimanantsoa@bocasay.com>
2023-10-29 19:54:40 +0300

    test

---
+++ src/components/input/WithIconInput.tsx
@@ -0,0 +1,56 @@
+type PropsWithIconInput = {
+  label?: string | null;
+  type?: React.HTMLInputTypeAttribute;
+  children?: React.ReactNode;
+  placeholder?: string;
+  value?: string | number | readonly string[];
+};
+export default function WithIconInput({
+  label = null,
+  type = "text",
+  children,
+  placeholder,
+  value,
+}: PropsWithIconInput) {
+  return (
+    <div>
+      <label
+        htmlFor="input-group-1"
+        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+      >
+        {label}
+      </label>
+      <div className="relative mb-6">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3.5 pointer-events-none">
+          {children ? (
+            children
+          ) : (
+            <svg
+              className="w-4 h-4 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 16"
+            >
+              <path d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z" />
+              <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z" />
+            </svg>
+          )}
+        </div>
+        <input
+          type={type}
+          id="input-group-1"
+          className="bg-gray-50 border border-gray-300
+           text-gray-900 text-sm rounded-lg  focus:border-violet-1 
+            block w-full pl-10 p-2.5 
+            appearance-none 
+            outline-none
+            focus:outline-none focus:ring-0
+            "
+          placeholder={placeholder}
+          value={value}
+        />
+      </div>
+    </div>
+  );
+}
